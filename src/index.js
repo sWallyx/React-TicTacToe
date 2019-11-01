0036f82f7c712a943b1335345fae49ac1b0ea074
sWallyx <mikelsmartinez@aol.com>
2019-11-01 20:05:47 +0000

    check and anounce winner

--- src/index.js
+++ src/index.js
@@ -35,6 +35,12 @@ class Board extends React.Component {
 
     handleClick(i) {
         const squares = this.state.squares.slice();
+
+        /* Check if game ended or not more squares */
+        if (calculateWinner(squares) || squares[i]) {
+            /* Cancel handle */
+            return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O'; /* Depending on the turn, X or O is placed */
         this.setState({
             squares: squares,
@@ -48,8 +54,17 @@ class Board extends React.Component {
     }
 
     render() {
-        /* Text status with user control, on single line if */
-        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        /* Check if winner */
+        const winner = calculateWinner(this.state.squares);
+        let status;
+
+        /* Set winner or continue playing */
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            /* Text status with user control, on single line if */
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
         return (
             <div>
@@ -96,3 +111,29 @@ ReactDOM.render(
     <Game />,
     document.getElementById('root')
 );
+
+
+/* Function to calculate the winner of the game */
+function calculateWinner(squares) {
+
+    /* Posible winning lines to check */
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    /* Check if result win */
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
